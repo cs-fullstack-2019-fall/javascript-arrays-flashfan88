d6dc23c7eb17e9e480bcef26e37d9eb5f61b1890
flashfan88 <lavarjohnson20@gmail.com>
2019-08-13 12:13:13 -0500

    itried

---
+++ index.js
@@ -0,0 +1,28 @@
+// Exercise 1
+// Create an array.
+//     Add four names to the array.
+//     Print the array
+
+// let names = ["Tweddle ", "Dee ", "Tweddle ", "Dum "];
+// alert(names);
+
+// Exercise 2
+// Create an array with 5 numbers in it.
+//     Print the numbers.
+
+// let numberS = ["5","10","15","20","25"];
+// alert(numberS);
+
+
+// Exercise 3
+// Create an array with 4 names.
+//     Print the third item in the array.
+//     Delete the second item.
+//     Print the third item in the array again.
+
+let heroeS = ["Thor","Hulk","Ironman","Hawkeye"];
+var first = heroeS[0];
+console.log(heroeS);
+splice[1];
+
+
